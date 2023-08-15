284248a571acade54e4c923ea841469865c9bed0
Yasin Cakal <whoisyasin@gmail.com>
2023-08-15 15:07:31 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import Form from './Form'
+import List from './List'
+
+function App() {
+  const API_URL = 'https://jsonplaceholder.typicode.com/'
+  const [reqType, setReqType] = useState('users')
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(`${API_URL}${reqType}`)
+        const data = await response.json()
+        setItems(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchItems()
+  }, [reqType])
+
+  return (
+    <>
+      <Form
+        reqType={reqType}
+        setReqType={setReqType}
+      />
+      <List items={items} />
+    </>
+  )
+}
+
+export default App
